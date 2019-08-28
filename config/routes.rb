2ba04e7c1c05d6cc9d5c1325275d2948d22385af
lsfernandes92 <lsfernandes92@gmail.com>
2019-08-27 20:59:13 -0700

    Enable login and logout feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
   root 'static_pages#home'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users, only: [:create, :show]
 end
